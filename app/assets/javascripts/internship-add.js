0f3708fff138a1cfd6f21b799f45c2dcfdd450d6
Celso Dantas <celsodantas@gmail.com>
2011-09-30 00:35:01 -0300

    refactoring! now using UJS! =D

--- app/assets/javascripts/internship-add.js
+++ app/assets/javascripts/internship-add.js
@@ -3,56 +3,6 @@ $(function () {
 		$("#btn-new-internship").click();
 	})
 	
-	function resetForm() {
-		var title = $("#box-add-internship input.title");
-		var area = $("#box-add-internship input.area");
-		var desc = $("#box-add-internship textarea.description");
-		
-		title.val(title.attr("original"));
-		area.val(area.attr("original"));
-		desc.val(desc.attr("original"));
-
-		title.removeClass("black-txt");
-		area.removeClass("black-txt");
-		desc.removeClass("black-txt");
-		
-		$("#errors").html("")
-	}
-	
-	function setDisabled (e) { e.attr("disabled", "disabled") }
-	function setEnabled (e)  { e.removeAttr("disabled") }
-	
-	function showNewInternshipBox(after) {
-		var btn = $("#btn-new-internship");		
-		var box = $("#box-add-internship");
-		
-		$(btn).addClass("pressed")
-		resetForm();
-		setDisabled(btn)
-		
-		box.wakShow(function () { 
-			setEnabled(btn); 
-			if (after) after() 
-		});
-		
-		box.find("input[type='submit']").removeAttr("disabled")
-	}
-	
-	function hideNewInternshipBox(after) {
-		var btn = $("#btn-new-internship");
-		var box = $("#box-add-internship");
-		
-		btn.removeClass("pressed");
-		setDisabled(btn);
-		
-		box.wakHide(function () { 
-			setEnabled(btn); 
-			if (after) after();
-		});
-		
-		box.find("input[type='submit']").attr("disabled", "disabled")
-	}
-
 	$("#btn-new-internship").click(function () {
 		var box = $("#box-add-internship");
 
@@ -97,89 +47,57 @@ $(function () {
 			maxCount: 1500, strictMax: true})
 	$(counter).html(1500)	// Fixing view, so it shows 800 
 							// and not count with the example.
+});
 
-	$("#internship_create").live("submit", function () {
-		var form = $(this);
-		var ajax = form.find(".ajax-loader");
-		var send_button = form.find("input[type=submit]");
-		var success_msg	= form.find(".done");
-		
-		send_button.hide();
-		ajax.show();
-		
-		if (valid()) {
-			post($(this).attr("action"), 
-		    	 $(this).serialize(), 
-				 function (data) {
-					addInternshipInView(data);
-				
-					ajax.hide();
-					success_msg.show();
-				
-					hideNewInternshipBox(function() {
-						success_msg.hide();
-						send_button.show();
-					});
-				}, function (err) {
-					var errors = $.parseJSON(err.responseText);
-					for(var e in errors) {
-						$("#errors").html(errors[e][0])
-					}
-					
-					ajax.hide();
-					send_button.show();
-				});
-			
-		}
-	    return false;
-	})
+//
+// Global functions
+//
+function hideNewInternshipBox (after) {
+	var btn = $("#btn-new-internship");
+	var box = $("#box-add-internship");
 	
-	function post(url, data, onsucess, onerror) {
-		$.ajax({type: 'POST', url: url, data: data, success: onsucess, error: onerror })
-	}
+	btn.removeClass("pressed");
+	setDisabled(btn);
 	
-	function addInternshipInView(internship) {
-		var i = internship;
-		scrollToTop(function() {
-			internship = $(internship).css("display", "none");
+	box.wakHide(function () { 
+		setEnabled(btn); 
+		if (after) after();
+	});
+	
+	box.find("input[type='submit']").attr("disabled", "disabled")
+}
 
-			addOnTop(internship);
-			$(internship).slideDown(1000);
+function resetForm() {
+	var title = $("#box-add-internship input.title");
+	var area = $("#box-add-internship input.area");
+	var desc = $("#box-add-internship textarea.description");
 
-			removeLast();
-		});
-	}
-	
-	function scrollToTop(exec_after) {
-		$('html').animate({ scrollTop: 0 }, 1000, exec_after);
-	}
-	
-	function addOnTop(internship) {
-		$(".right-content .content article:first").before(internship);
-	}
-	
-	function removeLast () {
-		$(".right-content .content article:last").remove();
-	}
-	
-	var $form_error = "";
-	function valid() {
-		var title = $("#box-add-internship input.title");
-		var area = $("#box-add-internship input.area");
-		var desc = $("#box-add-internship textarea.description");
-		
-		var v = true;
-		
-		if (title.val() == title.attr("original")) {
-			title.animate({ backgroundColor: "#ffd5d5", color : "black"}, 1500);
-			v = false;}
-		if (area.val() == area.attr("original")) {
-			area.animate({ backgroundColor: "#ffd5d5", color : "black"}, 1500);
-			v = false; }
-		if (desc.val() == desc.attr("original")) {
-			desc.animate({ backgroundColor: "#ffd5d5", color : "black"}, 1500);
-			v = false; }
-		
-		return v;
-	}
-})
+	title.val(title.attr("original"));
+	area.val(area.attr("original"));
+	desc.val(desc.attr("original"));
+
+	title.removeClass("black-txt");
+	area.removeClass("black-txt");
+	desc.removeClass("black-txt");
+
+	$("#errors").html("")
+}
+
+function setDisabled (e) { e.attr("disabled", "disabled") }
+function setEnabled (e)  { e.removeAttr("disabled") }
+
+function showNewInternshipBox(after) {
+	var btn = $("#btn-new-internship");		
+	var box = $("#box-add-internship");
+
+	$(btn).addClass("pressed")
+	resetForm();
+	setDisabled(btn)
+
+	box.wakShow(function () { 
+		setEnabled(btn); 
+		if (after) after() 
+	});
+
+	box.find("input[type='submit']").removeAttr("disabled")
+}
