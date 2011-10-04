425b039b01ff401e0ac8a19761b77ca4eebf0d9a
Celso Dantas <celsodantas@gmail.com>
2011-10-03 22:27:21 -0300

    fixing gravatar img size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   def gravatar_url(comment)
     default_url = "#{root_url}images/guest.png"
     gravatar_id = Digest::MD5.hexdigest(comment.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=40&d=#{CGI.escape(default_url)}"
   end
   
 end
